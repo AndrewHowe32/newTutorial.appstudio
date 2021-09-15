3e0cc6c05df2eda8a2bc139ffcc98605a736c3c8
AndrewHowe32 <andrewhowe@creighton.edu>
2021-09-15 14:15:03 -0500

    Added first and last name code

--- forms/Pets/Pets.js
+++ forms/Pets/Pets.js
@@ -1,3 +1,10 @@
 //This is the original first version
 
 console.log(`This is a test for the tutorial demo.`)
+
+// New code version two
+
+let myFirstName = prompt("Enter your first name: ")
+console.log(`Your first name is ${myFirstName}.`)
+let myLastName = prompt("Enter your last name: ")
+console.log(`Your first name is ${myLastName}.`)
